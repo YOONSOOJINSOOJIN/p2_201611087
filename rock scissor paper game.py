a43b17a3239566a050ad5a1c7758ce7afcbcc63b
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-04-03 21:12:24 +0900

    Added files via upload

---
+++ rock scissor paper game.py
@@ -0,0 +1,47 @@
+﻿
+
+userA=raw_input('userA input scissor, rock, paper : ')
+
+userB=raw_input('userB input scissor, rock, paper : ')
+
+if userA=='scissor':
+
+    if userB=='rock':
+
+        print "%s" %"UserB Win"
+
+    elif userB=='paper':
+
+        print "%s" %"UserA Win"
+
+    else:
+
+        print "%s" %"Draw"
+
+elif userA=='rock':
+
+    if userB=='scissor':
+
+        print "%s" %"UserA Win"
+
+    elif userB=='paper':
+
+        print "%s" %"UserB Win"
+
+    else:
+
+        print "%s" %"Draw"
+
+elif userA=='paper':
+
+    if userB=='scissor':
+
+        print "%s" %"UserB Win"
+
+    elif userB=='rock':
+
+        print "%s" %"UserA Win"
+
+    else:
+
+        print "%s" %"Draw"
