8d28acc7e194da55584f20aa9a023e1dd7019af8
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-04-05 14:14:08 +0900

    Added files via upload

---
+++ w5WMain(BMI calculate).py
@@ -0,0 +1,25 @@
+﻿height=input ("input user height (m):")
+
+weight=input ("input user weight (kg):")
+
+BMI=weight/(height*height)
+
+if BMI<=18.5:
+
+    print "Underweight"
+
+elif 18.5<BMI<=23:
+
+    print "Normalweight"
+
+elif 23<BMI<=25:
+
+    print "Overweight"
+
+elif 25<BMI<=30:
+
+    print "Obesity"
+
+elif BMI>30:
+
+    print "An extremly obese"
