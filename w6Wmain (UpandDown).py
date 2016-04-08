e6f75ff768593f39c2b11c6ba73d2294b181691d
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-04-08 11:16:36 +0900

    Added files via upload

---
+++ w6Wmain (UpandDown).py
@@ -0,0 +1,35 @@
+﻿def UpandDown():
+
+    soojin1=raw_input("Please write number : ")
+
+    opportunity=raw_input("Please determine opportunity number : ")
+
+    opportunity=int(opportunity)
+
+    for i in range(opportunity):
+
+        soojin1=int(soojin1)
+
+        soojin2=raw_input("Please write number : ")
+
+        soojin2=int(soojin2)
+
+        if soojin1>soojin2:
+
+            print "Up"
+
+        elif soojin1<soojin2:
+
+            print "Down"
+
+        elif soojin1==soojin2:
+
+            print "Good!"
+
+            break
+
+        print "Game over"
+
+ 
+
+UpandDown()
---
+++ w6Wmain (leap year).py
@@ -0,0 +1,17 @@
+﻿def leapyear():
+
+    year=raw_input("Please write year : ")
+
+    year=int(year)
+
+    if (year%4 == 0) and (year%100 != 0 or year%400== 0):
+
+        print "leap year"
+
+    else :
+
+        print "not leap year"
+
+ 
+
+leapyear()
