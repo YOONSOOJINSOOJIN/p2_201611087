18bc5a3b653583047f425b56d0e2d5b993f47101
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-04-08 11:29:46 +0900

    Update w6Wmain (leap year).py

--- w6Wmain (leap year).py
+++ w6Wmain (leap year).py
@@ -15,3 +15,74 @@
  
 
 leapyear()
+"""
+@startuml
+ 
+
+ 
+
+ 
+
+ 
+title Leap Year
+ 
+
+ 
+ 
+ 
+
+ 
+start
+ 
+
+ 
+:user input year;
+ 
+
+ 
+ 
+ 
+
+ 
+if(year%4==0)and (year%100 !=0 or year%400==0) then (yes)
+ 
+
+ 
+ 
+ 
+
+:print Leap Year;
+ 
+
+ 
+
+ 
+
+ 
+else (no)
+ 
+
+ 
+
+ 
+
+ 
+:print Not Leap Year;
+ 
+
+ 
+
+
+ 
+endif
+"""
+
+def lab6():
+    leapyear()
+    
+def main():
+    lab6()
+if__name__=="__main__":
+    main()
+    
+ 
