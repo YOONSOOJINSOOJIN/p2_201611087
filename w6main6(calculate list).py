2228a0e187c34fda8df116c031a1b202df8352ac
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-04-12 00:05:07 +0900

    Added files via upload

---
+++ w6main6(calculate list).py
@@ -0,0 +1,41 @@
+﻿def totalList(x):
+
+    y=list()
+
+    for i in range(0,x):
+
+        if (i%4==0 and not i%5==0):
+
+            y.append(i)
+
+            
+
+    total=0
+
+    for i in range(0,len(y)):
+
+        total+=y[i]
+
+    return total
+
+    print total
+
+    
+
+def lab6():
+
+    print """201611087 yoonsoojin homework"""
+
+    x=1000
+
+    labtotal=totalList(x)
+
+    print labtotal
+
+    
+
+def main():
+
+    lab6()
+
+main()
