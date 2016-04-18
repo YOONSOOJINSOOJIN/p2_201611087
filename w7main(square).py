004ae80705802035f7f0c1d59da0eb30d042a823
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-04-18 16:51:00 +0900

    Added files via upload

---
+++ w7main(square).py
@@ -0,0 +1,31 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+tracks=dict()
+
+def drawSquareFrom():
+
+    global tracks
+
+tracks=({1:(100,0),2:(100,100),3:(0,100),4:(0,0)})
+
+def lab7():
+
+    for i in range(1,5):
+
+        t1.goto(tracks[i])
+
+def main():
+
+    lab7()
+
+    
+
+main()
+
+    
+
+wn.exitonclick()
---
+++ w7main.py
@@ -0,0 +1,42 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+def drawSquareAt(size, pos):
+
+    t1.penup()
+
+    t1.setpos(pos)
+
+    t1.pendown()
+
+    tracks=list()
+
+    for i in range(0,4):
+
+        tracks.append(t1.pos())
+
+        t1.forward(size)
+
+        t1.right(90)
+
+    return tracks
+
+def lab7():
+
+    mytracks=drawSquareAt(100,(0,0))
+
+    print mytracks
+
+    
+
+def main():
+
+    lab7()
+
+ 
+
+main()
+
