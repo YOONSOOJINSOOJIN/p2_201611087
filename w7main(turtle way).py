fdb431e6e46e2e4d0b1de5208fc201174c38f3e8
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-04-20 14:15:21 +0900

    Update w7main(turtle way).py

--- w7main(turtle way).py
+++ w7main(turtle way).py
@@ -22,23 +22,23 @@ def saveTracks():
 
     t1.right(90)
 
-    mytracks.append(t1.pos())
-
     t1.fd(400) 
 
+    mytracks.append(t1.pos())
+    
     t1.backward(150) 
 
     t1.left(90) 
 
-    mytracks.append(t1.pos())
-
     t1.fd(300) 
+    
+    mytracks.append(t1.pos())
 
     t1.left(90) 
 
-    mytracks.append(t1.pos())
-
     t1.fd(300) 
+    
+    mytracks.append(t1.pos())
 
     t1.back(150) 
 
@@ -48,15 +48,17 @@ def saveTracks():
 
     t1.fd(300) 
 
+    mytracks.append(t1.pos())
+     
     t1.left(90) 
 
     t1.right(90) 
 
     t1.right(90) 
 
-    mytracks.append(t1.pos())
-
     t1.fd(200) 
+    
+    mytracks.append(t1.pos())
 
     t1.fd(300) 
 
