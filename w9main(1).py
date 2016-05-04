2a24707e1d893bf7eec78e420ad8709f3916e73a
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-05-04 11:12:14 +0900

    문자열의 문자 갯수 세기

---
+++ w9main(1).py
@@ -0,0 +1,47 @@
+﻿def charCount():
+
+    sentence= 'sangmyung university'
+
+    print list(sentence)
+
+    allchars=list(sentence)
+
+    for c in allchars:
+
+        print c,
+
+    d=dict()
+
+    for c in allchars:
+
+        if c not in d:
+
+            d[c]=1
+
+    else:
+
+            d[c]=d[c]+1
+
+    print d
+
+    import matplotlib
+
+    import matplotlib.pyplot as plt
+
+ 
+
+    plt.bar(range(len(d)),d.values(), align='center')
+
+    plt.xticks(range(len(d)), list(d.keys()))
+
+    plt.show()
+
+ 
+
+def main():
+
+    lab9()
+
+    
+
+    main()
