e650a4a69e265b544c7fdeb47d2ef21e4d391400
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-05-04 11:13:14 +0900

    우리 집에 없지만 친구 집에 있는 가전제품 찾기

---
+++ w9main(3).py
@@ -0,0 +1,81 @@
+﻿myhome=set()
+
+friendshome=set()
+
+myhome={'TV', 'phone', 'camera', 'fridger', 'mixer', 'audio', 'cd player', 'light' , 'computer', 'notebook', 'recorder'}
+
+friendshome={'coffee', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook', 'recorder'}
+
+ 
+
+def myhome():
+
+    print myhome.difference(friendshome)
+
+def friendshome():
+
+    print friendshome.difference(myhome)
+
+def together():
+
+    print myhome.intersection(friendshome)
+
+def All():
+
+    print myhome.union(friendshome)
+
+def count():
+
+    d=dict()
+
+    for c in myhome:
+
+        if c not in d:
+
+            d[c]=1
+
+        else:
+
+            d[c]=d[c]+1
+
+    for c in friendshome:
+
+        if c not in d:
+
+            d[c]=1
+
+        else :
+
+            d[c]=d[c]+1
+
+    print d 
+
+ 
+
+    
+
+def lab9():
+
+    myhome()
+
+    friendshome()
+
+    together()
+
+    All()
+
+    count()
+
+    
+
+def main():
+
+    lab9()
+
+    
+
+ 
+
+    
+
+    main()
