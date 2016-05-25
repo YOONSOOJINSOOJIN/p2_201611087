97f0262e1681199267566d07e89ccd323a1f490f
YOONSOOJINSOOJIN <xbdnlxl@hanmail.net>
2016-05-25 13:50:53 +0900

    Add files via upload

---
+++ w12main(2).py
@@ -0,0 +1,25 @@
+﻿def msg():   
+    msg='[ysj edited {0}]'.format(time.strftime('%Y-%m-%d %H:%M:%S'))
+print msg()
+ 
+def data():
+    data=[1,2,3,4,5,6,7,8,9,10]
+    fout=open('outputNumber.txt','w')
+    for i in data:
+        str="{0}\t".format(i)
+        if not i%2:
+            str=str+'\n'
+        fout.write(str)
+    fout.close()
+    
+print data()
+ 
+def lab12():
+    msg()
+    data()
+    
+def main():
+    lab12()
+    
+if __name__=="__main__":
+    main()
